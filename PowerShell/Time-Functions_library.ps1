addc2faee47f2cc37b33a4f8c1ffc4cd1b80267f
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-09-24 02:03:40 -0700

    r0.0051a
    Building more functions for Time-Functions_library
    
    WARNING: This commit has been modified posthumously using
    
    git rebase -i HEAD~n
    
    where n is the number of past commits to be retrieved, e.g.
    
    git rebase -i HEAD~5
    
    for the past 5 commits. Then it was renamed from r0.0050a to r0.0051a
    using reword command.
    
    See:
    https://help.github.com/en/articles/changing-a-commit-message
    
    for more info.

--- PowerShell/Time-Functions_library.ps1
+++ PowerShell/Time-Functions_library.ps1
@@ -1,4 +1,10 @@
 ﻿
+#
+
+. "$env:UserProfile\Documents\GitHub\Batch-Tools-SysAdmin\PowerShell\powershell-template.ps1" -LoadFunctions
+
+#
+
 $TodayDay = Get-Date -UFormat %d
 
 $TodayDay = Get-Date
@@ -19,25 +25,24 @@ do {
 	$ChoiceAMPM24hour = Read-Host -Prompt "[A]M, [P]M, or [2]4 hour? [A\P\2]"
 	switch ($ChoiceAMPM24hour) {
 		'A'	{ # A - AM time
-            $StartAMPM
+            $StartAMPM = "AM"
 			Write-Host "AM time ('$ChoiceAMPM24hour') option selected."
 			Write-Verbose "AM time ('$ChoiceAMPM24hour') option selected."
 			Write-Host `r`n
 		}
 		'P' { # P - PM time
-            $StartAMPM
+            $StartAMPM = "PM"
 			Write-Host "PM time ('$ChoiceAMPM24hour') option selected."
 			Write-Verbose "PM time ('$ChoiceAMPM24hour') option selected."
 			Write-Host `r`n
 		}
 		2 { # 2 - 24-hour time
-            $StartAMPM
+            $StartAMPM = "24"
 			Write-Host "24-hour time ('$ChoiceAMPM24hour') option selected."
 			Write-Verbose "24-hour time ('$ChoiceAMPM24hour') option selected."
 			Write-Host `r`n
 		}
 		default { # Choice not recognized.
-			$StartAMPM
             Write-Host `r`n
 			Write-Host "Choice `"$ChoiceAMPM24hour`" not recognized. Options must be AM, PM, or 24-hour."
 			Write-Host `r`n
@@ -49,67 +54,21 @@ do {
 }
 until ($ChoiceAMPM24hour -eq 'A' -Or $ChoiceAMPM24hour -eq 'P' -Or $ChoiceAMPM24hour -eq 2)
 
-$StartAMPM
+If ($StartAMPM -eq "AM") {
+    $24hour = Convert-AMPMhourTo24hour $StartHour -AM
+}
 
+If ($StartAMPM -eq "PM") {
+    $24hour = Convert-AMPMhourTo24hour $StartHour -PM
+}
 
-#doesn't work
-$Midnight = Get-Date -Hour 24
+If ($StartAMPM -eq 24) {
+    $24hour = $StartHour
+}
 
-$Midnight
+$Timestamp = Get-Date -Hour $24hour -Minute $StartMin -Second 0 -Millisecond 0
 
-
-$Midnight = Get-Date -Hour 23
-
-$Midnight
-
-
-$PastMidnight = Get-Date -Hour 0
-
-$PastMidnight
-
-$PastMidnight = Get-Date -Hour 1
-
-$PastMidnight
-
-  
-
-
-:: We now have the time in 24-hour hh-mm-ss format e.g. 21-31-39 or 01-57-25
-::ECHO Sortable time = "%_TIME_SORT%"
-SET "_TIME_ANTE_POST=AM"
-:: Extract hours
-SET "_TIME_24HOUR=%_TIME_SORT:~0,2%"
-IF %_TIME_24HOUR% LSS 10 SET "_TIME_24HOUR=%_TIME_24HOUR:~1%"
-:: https://ss64.com/nt/if.html
-IF %_TIME_24HOUR% GEQ 12 (
-	SET "_TIME_ANTE_POST=PM"
-	REM https://ss64.com/nt/set.html
-	IF %_TIME_24HOUR% GTR 12 (
-		SET /A "_TIME_12HOUR=_TIME_24HOUR-12"
-	) ELSE (
-		SET "_TIME_12HOUR=%_TIME_24HOUR%"
-	)
-) ELSE (
-	SET "_TIME_12HOUR=%_TIME_24HOUR%"
-)
-:: If 24hr time 'hours' is 00, change to 12 (for 12:00 AM)
-IF %_TIME_24HOUR% EQU 0 SET "_TIME_12HOUR=12"
-:: Add spaces
-IF %_TIME_12HOUR% LSS 10 SET "_TIME_12HOUR= %_TIME_12HOUR%"
-:: Skip the hours and extract the rest of :mm:ss from hh:mm:ss e.g. :31:39
-SET "_TIME_COLONS=%_TIME_SML:~2%"
-SET "_TIME_AMPM=%_TIME_12HOUR%%_TIME_COLONS% %_TIME_ANTE_POST%"
-::ECHO 12-hour time with spaces = "%_TIME_AMPM%"
-:: We now have the time in 12-hour hh:mm:ss AM/PM format e.g. " 9:31:39 PM"
-
-
-
-
-
-
-
-
-$StartTime = Get-Date -Hour $StartHour -Minute $StartMin
+$StartTime = Get-Date -Hour $24hour -Minute $StartMin -Second 0 -Millisecond 0
 
 $StartTime
 
@@ -119,9 +78,23 @@ $EndHour = Read-Host -Prompt "Enter End hour"
 
 $EndMin = Read-Host -Prompt "Enter End minute"
 
-$EndAMPM
+$EndAMPM = ReadPrompt-AMPM24
 
-$EndTime = Get-Date -Hour $EndHour -Minute $EndMin
+If ($EndAMPM -eq "AM") {
+    $24hour = Convert-AMPMhourTo24hour $EndHour -AM
+}
+
+If ($EndAMPM -eq "PM") {
+    $24hour = Convert-AMPMhourTo24hour $EndHour -PM
+}
+
+If ($EndAMPM -eq 24) {
+    $24hour = $EndHour
+}
+
+$Timestamp = Get-Date -Hour $24hour -Minute $EndMin -Second 0 -Millisecond 0
+
+$EndTime = Get-Date -Hour $24hour -Minute $EndMin -Second 0 -Millisecond 0
 
 $EndTime
 
--- PowerShell/powershell-template.ps1
+++ PowerShell/powershell-template.ps1
@@ -655,6 +655,52 @@ Function Write-HorizontalRuleAdv {
 } # End Write-HorizontalRuleAdv function -------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 
+
+
+Function ReadPrompt-AMPM24 {
+	
+	do {
+		$ChoiceAMPM24hour = Read-Host -Prompt "[A]M, [P]M, or [2]4 hour? [A\P\2]"
+		switch ($ChoiceAMPM24hour) {
+			'A'	{ # A - AM time
+	            $ResultAMPM = "AM"
+				Write-Host "AM time ('$ChoiceAMPM24hour') option selected."
+				Write-Verbose "AM time ('$ChoiceAMPM24hour') option selected."
+				Write-Host `r`n
+			}
+			'P' { # P - PM time
+	            $ResultAMPM = "PM"
+				Write-Host "PM time ('$ChoiceAMPM24hour') option selected."
+				Write-Verbose "PM time ('$ChoiceAMPM24hour') option selected."
+				Write-Host `r`n
+			}
+			2 { # 2 - 24-hour time
+	            $ResultAMPM = "24"
+				Write-Host "24-hour time ('$ChoiceAMPM24hour') option selected."
+				Write-Verbose "24-hour time ('$ChoiceAMPM24hour') option selected."
+				Write-Host `r`n
+			}
+			default { # Choice not recognized.
+	            Write-Host `r`n
+				Write-Host "Choice `"$ChoiceAMPM24hour`" not recognized. Options must be AM, PM, or 24-hour."
+				Write-Host `r`n
+				#Break #help about_Break
+				PAUSE # PAUSE (alias for Read-Host) Prints "Press Enter to continue...: "
+				Write-Host `r`n
+			}
+		}
+	}
+	until ($ChoiceAMPM24hour -eq 'A' -Or $ChoiceAMPM24hour -eq 'P' -Or $ChoiceAMPM24hour -eq 2)
+	
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	Return $ResultAMPM
+	
+} # End ReadPrompt-AMPM24 function -------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+
 Function Convert-AMPMhourTo24hour {
 <#
 .SYNOPSIS
