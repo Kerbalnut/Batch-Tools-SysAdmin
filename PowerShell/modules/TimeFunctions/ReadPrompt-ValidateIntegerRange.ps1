b05bf0a6e37ce7de28d3dad3be773da0a735efd2
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-11-07 01:19:22 -0700

    module-r0.0137a - initiating Pester automated tests
    Got our "Throw" tests working!

--- PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.ps1
+++ PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.ps1
@@ -432,7 +432,9 @@ Function Read-ValidateInteger { #-----------------------------------------------
 			ValueFromPipeline = $true)]
 			$VarInput
 		)
-		$VarSimplified = $VarInput.TrimStart('0')
+		
+        $VarSimplified = $VarInput.ToString().TrimStart('0')
+        
 		If ($VarSimplified -eq $null) {
 			Write-Verbose "$VarName is `$null after removing leading zeros."
 			$VarSimplified = '0'
@@ -455,55 +457,17 @@ Function Read-ValidateInteger { #-----------------------------------------------
 	
 	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	# Check if we have a value sent in from an external variable (parameter) first
-	If ($ValueInput -eq $null -or $ValueInput -eq "") {
-		$PipelineInput = $false
-	} else {
-		$PipelineInput = $true
-		Write-Verbose "Piped-in content = $ValueInput"
-		$ValueInput = [string]$ValueInput #Bugfix: convert input from an object to a string
-	}
-	
-	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	# Initialize test verification vars
-	$IntegerValidation = $false
-	$RangeValidation = $false
-	
-	# Begin loop to validate Input, and request new input from user if it fails validation.
-	while ($IntegerValidation -eq $false -Or $RangeValidation -eq $false) {
-		
-		# Initialize test verification vars (at the start of each loop)
-		$IntegerValidation = $false
-		$RangeValidation = $false
-		
-		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		
-		# Prompt user for $VarName value input
-		If ($PipelineInput -ne $true) {
-			Write-Verbose "No values piped-in from external sources (parameters)"
-			$ValueInput = Read-Host -Prompt "Enter $VarName"
-			Write-Verbose "Entered value = $ValueInput"
-		} else {
-			Write-Verbose "Using piped-in value from parameter = $ValueInput"
-			$PipelineInput = $false
-		}
-		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
 		# Check if input is null
-		If ($ValueInput -eq $null -or $ValueInput -eq "") {
-			Write-HorizontalRuleAdv -DashedLine -IsWarning
-			Write-Warning "$VarName input is null."
-			#PAUSE
-			Write-Host `r`n
-			Continue #help about_Continue
+		If ($ValueInput -eq $null -Or $ValueInput -eq "" -Or $ValueInput -eq '') {
+			Throw "`$ValueInput is either Null or an empty string."
 		}
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
 		# Remove leading zeros (0)
-		$VarSimplified = Remove-LeadingZeros $ValueInput
+		$VarSimplified = Remove-LeadingZeros -VarInput $ValueInput
 		Write-Verbose "Remove leading zeros (0) = $VarSimplified"
 		
 		# Remove leading zeros (0)
@@ -526,7 +490,7 @@ Function Read-ValidateInteger { #-----------------------------------------------
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
-		# Check if $VarName input is integer using Validate-Integer function
+		# Check if $ValueInput is integer using Validate-Integer function
 		try { # help about_Try_Catch_Finally
 			#https://stackoverflow.com/questions/6430382/powershell-detecting-errors-in-script-functions
 			$VarInteger = Validate-Integer $VarSimplified -ErrorVariable ValidateIntError
@@ -535,52 +499,23 @@ Function Read-ValidateInteger { #-----------------------------------------------
 			#https://devblogs.microsoft.com/scripting/handling-errors-the-powershell-way/
 		}
 		catch {
-			Write-HorizontalRuleAdv -DashedLine -IsVerbose
 			Write-Verbose "`$ValidateIntError:" # Error variable set using the -ErrorVariable "common parameter": Get-Help -Name about_CommonParameters
 			Write-Verbose "$ValidateIntError" -ErrorAction 'SilentlyContinue' # Error variable set using the -ErrorVariable "common parameter": Get-Help -Name about_CommonParameters
-			#Write-HorizontalRuleAdv -SingleLine -IsVerbose
-			#Write-Verbose "`$error:" # Command's error record will be appended to the "automatic variable" named $error
-			#Write-HorizontalRuleAdv -DashedLine -IsVerbose
-			#Write-Verbose "$error" -ErrorAction 'SilentlyContinue' # Command's error record will be appended to the "automatic variable" named $error
-			Write-HorizontalRuleAdv -DashedLine -IsWarning
-			Write-Warning "$VarName input must be an integer. (Whole numbers only, no decimals, no negatives.)"
-			#PAUSE
-			Write-Host `r`n
-			Continue #help about_Continue
+			Throw "`$ValueInput must be an integer. (Whole numbers only, no decimals, no negatives.)"
+            Return
 		}
-		
-		$IntegerValidation = $true
 		
 		Write-Verbose "Integer validation success = $VarInteger"
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
-		# Check if $VarName input is between $MinInt and $MaxInt
-		If ([int]$VarInteger -ge [int]$MinInt -And [int]$VarInteger -le [int]$MaxInt) {
-			$VarRange = [int]$VarInteger
-			$RangeValidation = $true
-		} else {
-			Write-HorizontalRuleAdv -DashedLine -IsWarning
-			Write-Warning "$VarName input must be between $MinInt-$MaxInt."
-			If (!($HintMinMax -eq $null -Or $HintMinMax -eq "")) {
-				Write-Warning $HintMinMax
-			}
-			#PAUSE
-			Write-Host `r`n
-			Continue #help about_Continue
-		}
-		
-		Write-Verbose "$VarName value range validation = $VarRange"
-		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
-	}
-	
 	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	Write-Verbose "$VarName value $VarRange validation complete."
+	Write-Verbose "`"$ValueInput`" value $VarInteger validation complete."
 	
-	Return [int]$VarRange
+	Return [int]$VarInteger
 	
 } # End ReadPrompt-ValidateIntegerRange function -----------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
--- PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.test.ps1
+++ PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.test.ps1
@@ -3,14 +3,8 @@
 
 # Dot source our function(s) to test.
 
-. "C:\Users\Grant\Documents\GitHub\Batch-Tools-SysAdmin\PowerShell\powershell-template.ps1" -LoadFunctions
+#. "C:\Users\Grant\Documents\GitHub\Batch-Tools-SysAdmin\PowerShell\powershell-template.ps1" -LoadFunctions
 . "$env:USERPROFILE\Documents\GitHub\Batch-Tools-SysAdmin\PowerShell\modules\TimeFunctions\ReadPrompt-ValidateIntegerRange.ps1"
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-#=======================================================================================================================
-#4. Testing ReadPrompt-Hour
-#=======================================================================================================================
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -19,7 +13,7 @@
 #https://devblogs.microsoft.com/scripting/what-is-pester-and-why-should-i-care/
 
 #=======================================================================================================================
-Describe 'Testing ReadPrompt-Hour' {
+Describe 'Read-ValidateInteger' {
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     #-----------------------------------------------------------------------------------------------------------------------
     
@@ -38,25 +32,22 @@ Describe 'Testing ReadPrompt-Hour' {
     
     Context ':: Piping in valid integer ranges. No user prompt should be expected. ::' {
 
-        #It 'Test3' {
-        It 'Integer 4, within range, with leading zeros (no quotes)' {
-            0000004 | ReadPrompt-Hour | Should Be 4
+        It 'Integer 4, with leading zeros (no quotes)' {
+            0000004 | Read-ValidateInteger | Should -Be 4
         }
         
         #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-        It 'Integer 4, within range, with leading zeros (single quotes)' {
-            '0000004' | ReadPrompt-Hour | Should Be 4
+        It 'Integer 4, with leading zeros (single quotes)' {
+            '0000004' | Read-ValidateInteger | Should -Be 4
         }
         
         #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-        It 'Integer 0, within range, with leading zeros (double quotes)' {
-            "0000000" | ReadPrompt-Hour | Should Be 0
+        It 'Integer 0, with leading zeros (double quotes)' {
+            "0000000" | Read-ValidateInteger | Should -Be 0
         }
         
-
-
     }
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -68,43 +59,32 @@ Describe 'Testing ReadPrompt-Hour' {
             #https://github.com/PowerShell/PowerShell/issues/2408#issuecomment-251140889
             ((Get-Command Read-ValidateInteger).Parameters['ValueInput'].Attributes | ? { $_ -is [parameter] }).Mandatory | Should Be $true
         }
-
-        #Mocks
         
-        Mock ReadPrompt-Hour { Return 0..23 }
-
-        It 'Integer, out-of-range' {
-            24 | ReadPrompt-Hour | Out-Null | Should Be $null
-            
+        #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        
+        It "Read-ValidateInteger - " {
+            {2.4 | Read-ValidateInteger} | Should -Throw
         }
         
-        It 'Integer, out-of-range' {
-            21 | ReadPrompt-Hour | Should Be $null
-            
-        }
-            <#
-Context 'MyModule' {
-    Mock -ModuleName MyModule Test-Foo { return 'C:\example' }
-
-    It 'gets user input' {
-        Test-Foo | Should -Be 'C:\example'
-    }
-}
-            #>
-
-
-
-        #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
-        It 'Decimal value, within range' {
-            2.4 | ReadPrompt-Hour | Should Throw
-        }
-    
         #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    }
+        It "Read-ValidateInteger - " {
+            {-2 | Read-ValidateInteger} | Should -Throw
+        }
+        
+        #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        It "Read-ValidateInteger - " {
+            {0.01 | Read-ValidateInteger} | Should -Throw
+        }
+        
+        #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        It "Read-ValidateInteger - " {
+            {-0000.0010 | Read-ValidateInteger} | Should -Throw
+        }
+        
+    }
 
     #-----------------------------------------------------------------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -112,101 +92,3 @@ Context 'MyModule' {
 #=======================================================================================================================
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-If ($true -eq $false) {
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-#
-
-Write-Host "--------------------------------------------------------------------------------------------------"
-
-#
-
-Write-Host `r`n
-Write-Host `r`n
-
-#
-
-Write-Host "# Start Time #`n`r`n"
-
-Write-Host `r`n
-
-#Write-HorizontalRuleAdv -SingleLine
-
-#$StartHour = Read-Host -Prompt "Enter Start hour"
-#$StartHour = ReadPrompt-Hour -Verbose
-$StartHour = ReadPrompt-Hour -Verbose
-
-#
-
-#Write-HorizontalRuleAdv
-
-#
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Success:" -ForegroundColor Green
-$StartHour = (0000004 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Success:" -ForegroundColor Green
-$StartHour = ('0000004' | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Success:" -ForegroundColor Green
-$StartHour = ("0000000" | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Failure:"
-$StartHour = (24 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Failure:"
-$StartHour = (2.4 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Failure:"
-$StartHour = (-2 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Failure:"
-$StartHour = (0.01 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-Write-Host "Failure:"
-$StartHour = (-0000.0010 | ReadPrompt-Hour -Verbose)
-
-#Write-HorizontalRuleAdv -DashedLine
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-#
-
-#Write-HorizontalRuleAdv
-
-#
-
-Write-Host `r`n
-
-#
-
-Write-Host "--------------------------------------------------------------------------------------------------"
-
-#
-
-PAUSE # PAUSE (alias for Read-Host) Prints "Press Enter to continue...: "
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
