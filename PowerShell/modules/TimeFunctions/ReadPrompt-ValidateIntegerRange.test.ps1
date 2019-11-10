17a34eb960a7253802e4beb2ffefaffd7d86554e
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-11-10 02:22:46 -0700

    module-r0.0141a - initiating Pester automated tests
    Updating tests

--- PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.test.ps1
+++ PowerShell/modules/TimeFunctions/ReadPrompt-ValidateIntegerRange.test.ps1
@@ -18,7 +18,7 @@ Describe 'Read-ValidateInteger' {
 	#-----------------------------------------------------------------------------------------------------------------------
 	
 	Context ':: Foobar ::' {
-		
+
 		It 'Test1' {
 			$true | Should Be $true
 		}
@@ -30,37 +30,120 @@ Describe 'Read-ValidateInteger' {
 	
 	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	Context ':: Piping in valid integer ranges. No user prompt should be expected. ::' {
-		
+	Context ':: Success tests. Piping in valid integer ranges. ::' {
+
 		It 'Integer 4, with leading zeros (no quotes)' {
 			0000004 | Read-ValidateInteger | Should -Be 4
 		}
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		
+
 		It 'Integer 4, with leading zeros (single quotes)' {
 			'0000004' | Read-ValidateInteger | Should -Be 4
 		}
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		
+
 		It 'Integer 0, with leading zeros (double quotes)' {
 			"0000000" | Read-ValidateInteger | Should -Be 0
 		}
 		
 	}
-	
+
 	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	Context ':: Negative tests, which will throw a terminating error ::' {
+	Context ':: Testing for mandatory parameter, input value. ::' {
 		
 		It "Has a Mandatory 'ValueInput' parameter. If no input is given, PowerShell will prompt user for the missing parameter." {
 			#https://stackoverflow.com/questions/45935954/testing-for-mandatory-parameters-with-pester
 			#https://github.com/PowerShell/PowerShell/issues/2408#issuecomment-251140889
 			((Get-Command Read-ValidateInteger).Parameters['ValueInput'].Attributes | ? { $_ -is [parameter] }).Mandatory | Should Be $true
 		}
+    }
+		
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	Context ':: Failure tests: setting named parameters as non-integer values, which will throw an error. ::' {
+		
+		It "Decimal 2.4" {
+			{Read-ValidateInteger -ValueInput 2.4} | Should -Throw
+		}
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative whole number -2" {
+			{Read-ValidateInteger -ValueInput -2} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative whole number with leading zeroes -00009" {
+			{Read-ValidateInteger -ValueInput -00009} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative decimal with leading zeroes -00009.1" {
+			{Read-ValidateInteger -ValueInput -00009.1} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		It "Decimal with leading zero 0.01" {
+			{Read-ValidateInteger -ValueInput 0.01} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		It "Negative decimal with leading zeroes-0000.0010" {
+			{Read-ValidateInteger -ValueInput -0000.0010} | Should -Throw
+		}
+		
+	}
+
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	Context ':: Failure tests: setting positional parameters as non-integer values, which will throw an error. ::' {
+		
+		It "Decimal 2.4" {
+			{Read-ValidateInteger 2.4} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative whole number -2" {
+			{Read-ValidateInteger -2} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative whole number with leading zeroes -00009" {
+			{Read-ValidateInteger -00009} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		
+		It "Negative decimal with leading zeroes -00009.1" {
+			{Read-ValidateInteger -00009.1} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		It "Decimal with leading zero 0.01" {
+			{Read-ValidateInteger 0.01} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		It "Negative decimal with leading zeroes-0000.0010" {
+			{Read-ValidateInteger -0000.0010} | Should -Throw
+		}
+		
+	}
+
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	Context ':: Failure tests: piping in non-integer values which, will throw an error. ::' {
 		
 		It "Decimal 2.4" {
 			{2.4 | Read-ValidateInteger} | Should -Throw
@@ -80,18 +163,24 @@ Describe 'Read-ValidateInteger' {
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		
+		It "Negative decimal with leading zeroes -00009.1" {
+			{-00009.1 | Read-ValidateInteger} | Should -Throw
+		}
+		
+		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 		It "Decimal with leading zero 0.01" {
 			{0.01 | Read-ValidateInteger} | Should -Throw
 		}
 		
 		#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		
+
 		It "Negative decimal with leading zeroes-0000.0010" {
 			{-0000.0010 | Read-ValidateInteger} | Should -Throw
 		}
 		
 	}
-	
+
 	#-----------------------------------------------------------------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
