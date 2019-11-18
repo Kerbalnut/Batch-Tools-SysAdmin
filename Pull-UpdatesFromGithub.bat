0a963316c1db026c0627ff0ac90476f290b11732
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-11-18 01:24:44 -0700

    Testing command line input
    Testing if this is a message
    `

--- Pull-UpdatesFromGithub.bat
+++ Pull-UpdatesFromGithub.bat
@@ -1,9 +1,49 @@
 @ECHO OFF
 
+SET "_PREV_COMMITS_TO_GET=8"
+
+SET "_BRANCH=backup-and-restore"
+
 REM -------------------------------------------------------------------------------
 REM ===============================================================================
 REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 :Main
+
+:: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+::Thanks to:
+::https://git-scm.com/book/en/v2/Git-Basics-Viewing-the-Commit-History
+ECHO:
+ECHO Last %_PREV_COMMITS_TO_GET% commits in the local repository:
+ECHO:
+
+:: Natural command:
+::git log -6 --pretty=format:"%h - %an, %ar : %s"
+:: This will work from the command line, but in batch script percentage signs % must be doubled-up to be literal %%
+
+git log -%_PREV_COMMITS_TO_GET% --pretty=format:"%%h - %%an, %%ar : %%s"
+
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+:: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+::Thanks to:
+::https://stackoverflow.com/questions/180272/how-to-preview-git-pull-without-doing-fetch
+
+git fetch
+
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+ECHO:
+ECHO Newly-fetched commits from %_BRANCH%:
+ECHO:
+
+git -P log HEAD..origin/%_BRANCH%
+
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+PAUSE
 
 :: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
