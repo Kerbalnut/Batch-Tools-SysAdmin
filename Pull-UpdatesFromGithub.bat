6399fff3861a507d9a1b68a78224b87cc6c2418d
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-11-18 01:52:47 -0700

    Lots of changes here, so prob will be a sloppy merge, we'll see.

--- Pull-UpdatesFromGithub.bat
+++ Pull-UpdatesFromGithub.bat
@@ -16,7 +16,7 @@ ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ::Thanks to:
 ::https://git-scm.com/book/en/v2/Git-Basics-Viewing-the-Commit-History
 ECHO:
-ECHO Last %_PREV_COMMITS_TO_GET% commits in the local repository:
+ECHO Last %_PREV_COMMITS_TO_GET% commits known in the local repository:
 ECHO:
 
 :: Natural command:
@@ -25,6 +25,7 @@ ECHO:
 
 git log -%_PREV_COMMITS_TO_GET% --pretty=format:"%%h - %%an, %%ar : %%s"
 
+ECHO:
 ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 :: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -32,8 +33,9 @@ ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ::Thanks to:
 ::https://stackoverflow.com/questions/180272/how-to-preview-git-pull-without-doing-fetch
 
-git fetch
+::git fetch
 
+ECHO:
 ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 ECHO:
@@ -42,12 +44,77 @@ ECHO:
 
 git -P log HEAD..origin/%_BRANCH%
 
+ECHO:
 ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-PAUSE
 
 :: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+ECHO:
+ECHO Files changed in my local folder:
+ECHO:
+
+git -P diff --name-status
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+ECHO:
+ECHO Details of local changes:
+ECHO:
+PAUSE
+ECHO:
+
+git -P diff
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ECHO:
+
+:PromptForCommitMsg
+SET /P "_COMMIT_MESSAGE=Please enter a message about your changes: "
+IF /I "%_COMMIT_MESSAGE%"=="" GOTO PromptForCommitMsg
+
+ECHO "CMDLINE: %_COMMIT_MESSAGE%"
+
+PAUSE
+
+git add .
+git commit -m "CMDLINE: %_COMMIT_MESSAGE%"
+
+:: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ECHO:
+ECHO Merge:
+ECHO:
+PAUSE
+ECHO:
+
+git merge
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ECHO:
+ECHO Pull:
+ECHO:
+PAUSE
+ECHO:
+
 git pull
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ECHO:
+ECHO Push:
+ECHO:
+PAUSE
+ECHO:
+
+git push
+
+ECHO:
+ECHO - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 :: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
