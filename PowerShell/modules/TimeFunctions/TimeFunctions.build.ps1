74d61f64d0b7afdbe0dda5c649f638013e3ef3f4
Kerbalnut <Kerbalnut@users.noreply.github.com>
2019-11-22 21:28:36 -0700

    module-r0.0172a - Attempting build automation of Module
    Invoke-Build works! (and so does Invoke-Pester, look at that! And how!)

--- PowerShell/modules/TimeFunctions/TimeFunctions.build.ps1
+++ PowerShell/modules/TimeFunctions/TimeFunctions.build.ps1
@@ -10,5 +10,45 @@ https://github.com/nightroman/Invoke-Build
 http://duffney.io/GettingStartedWithInvokeBuild#powershell-module-development-workflow
 #>
 
-Invoke-Pester 
+#-----------------------------------------------------------------------------------------------------------------------
+#=======================================================================================================================
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+#Clear-Host # CLS
+#Start-Sleep -Milliseconds 100 #Bugfix: Clear-Host acts so quickly, sometimes it won't actually wipe the terminal properly. If you force it to wait, then after PowerShell will display any specially-formatted text properly.
+
+#=======================================================================================================================
+
+# Install dependencies
+
+task InstallDependencies
+
+task InstallDependencies {
+    Install-Module Pester -Force
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Test our cmdlets/libraries
+
+task Test {
+    $invokePesterParams = @{
+        Strict = $true
+        PassThru = $true
+        Verbose = $false
+        EnableExit = $false
+    }
+
+    # Publish Test Results as NUnitXml
+    $testResults = Invoke-Pester @invokePesterParams;
+
+    $numberFails = $testResults.FailedCount
+    assert($numberFails -eq 0) ('Failed "{0}" unit tests.' -f $numberFails)
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#=======================================================================================================================
+#-----------------------------------------------------------------------------------------------------------------------
