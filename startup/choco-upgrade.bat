bb8fdc42d30f834387e7505a39d85a6d62110d21
Kerbalnut <Kerbalnut@users.noreply.github.com>
2023-02-06 08:12:46 -0700

    Added startup folder for using chocolatey to automate software updates.

---
+++ startup/choco-upgrade.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+ECHO All outdated packages:
+
+choco outdated
+
+ECHO Upgrade packagaes:
+
+choco upgrade chocolatey -y
+choco upgrade discord -y
+choco upgrade Firefox -y
+choco upgrade git -y
+choco upgrade github-desktop -y
+choco upgrade GoogleChrome -y
+choco upgrade hg -y
+choco upgrade nextcloud-client -y
+choco upgrade notepadplusplus -y
+choco upgrade putty -y
+choco upgrade signal -y
+choco upgrade thunderbird -y
+choco upgrade vlc -y
+choco upgrade vscodium -y
+
+
